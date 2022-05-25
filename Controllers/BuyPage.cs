01f722ebe0f85f664103c7794ace6d68bf8ed517
Nataliia Shynkovska <natasha.dyachenko88@gmail.com>
2022-05-25 20:44:46 +0200

    Text

---
+++ Controllers/BuyPage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketsShop.Controllers
+{
+    public class BuyPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blik()
+        {
+            return View("Blik");
+        }
+
+        public IActionResult Mastercard()
+        {
+            return View("Mastercard");
+        }
+    }
+}
