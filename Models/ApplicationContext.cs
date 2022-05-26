74d46d328e1c71e0b028ece311c911b024a02eb1
Nataliia Shynkovska <natasha.dyachenko88@gmail.com>
2022-05-26 09:31:02 +0200

    text

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 
 namespace TicketsShop.Models
 {
@@ -35,24 +34,25 @@ namespace TicketsShop.Models
                 .HasForeignKey(item => item.CityId);
 
             var warszawa = new City { Id = 1, Name = "Warszawa" };
-            var katowice = new City { Id = 2, Name = "Katowice" };
+            var poznan = new City { Id = 2, Name = "Poznan" };
             var lublin = new City { Id = 3, Name = "Lublin" };
 
             modelBuilder.Entity<City>()
-                .HasData(warszawa, katowice, lublin);
+                .HasData(warszawa, poznan, lublin);
 
             var koncert = "Koncert";
             var teatr = "Teatr";
 
             var imagineDragons = new Event
             {
-                Name = "Imagine Dragons",
+                Name = "Ed Sheeran: +-=÷x Tour",
                 CityId = warszawa.Id,
                 CreationTime = DateTime.Now,
-                Description = "None",
-                EventTime = DateTime.Now,
+                Description = "Ed Sheeran ogłasza swoja najnowsza trasę ‘+ - = ÷ x Tour’ (wymawiane ‘The Mathematics Tour’/’Trasa Matematyczna’), która odbędzie się na stadionach w roku 2022. Tournée rozpocznie się w kwietniu przyszłego roku, a Ed zagra w UK, Irlandii, Środkowej Europie i Skandynawii, jak również powróci na londyński stadion Wembley, aby zagrać trzy koncerty w czerwcu/lipcu. Ogólna sprzedaż biletów na wszystkie kraje rozpocznie się w sobotę 25 września (dokładny czas w sprzedaży dla poszczególnych państw zamieszczony powyżej). Najbliższa trasa Eda ‘+ - = ÷ x Tour’ odbędzie się na stadionach po raz pierwszy od przełomowej ‘Divide Tour’ z lat 2017 – 2019. Była to oficjalnie najpopularniejsza trasa, która przyniosła najwyższe zyski jeszcze przed jej zakończeniem. Podczas przyszłorocznego tournée, fani będą mieli okazje zobaczyć Eda wykonującego po raz pierwszy na żywo wybór utworów z najnowszego albumu ‘=’. Będą mogli również doświadczyć wyjątkowej produkcji, z okrągłą sceną na środku stadionu, na której Ed wystąpi w każdym kraju otoczony publicznością. Wyczekiwany album Eda ‘=’ ukaże się 29 października nakładem Asylum/Atlantic. Oczekuje się, że nowy singiel artysty zajmie miejsce jego poprzedniego hitu na oficjalnej lisice singli w UK, utwór ‘Shivers’ – Ed wykonał go po raz pierwszy w poprzedni weekend na MTV VMA’s. Jest to kolejny szlagier po ogromnym sukcesie, ‘Bad Habits’, i powrocie Eda na listy, gdzie jak do tej pory, pozostawał w czołówce przez kolejne 11 tygodni. Po raz kolejny Ed i jego współpracownicy zdecydowanie przeciwstawiają się wtórnej sprzedaży biletów przez strony nieoficjalne, starając się uchronić fanów przez wyzyskiem w trakcie kupna. Koncerty na najnowszej trasie będą używać specjalnie zaprojektowanej biletowej technologii cyfrowej z zabezpieczeniami, aby mieć pewność, że tylko prawdziwi fani kupują autentyczne bilety. Ma to na celu zablokowanie wtórnej sprzedaży na stronach nieoficjalnych po wygórowanych cenach, co stanowi oszustwo wobec fanów. Aby usprawnić sprzedaż legalnych biletów, zachęcamy fanów do zarejestrowania się z wyprzedzeniem na konto oficjalnego sprzedawcy. Fani, którzy nie będą w stanie dotrzeć na koncert, będą mogli odsprzedać bilet innym fanom po cenie, za którą go nabyli + oficjalną opłatę rezerwacyjną poprzez oficjalną platformę do nominalnej odsprzedaży pomiędzy fanami, w miejscu, w którym bilet został nabyty.",
+                EventTime = DateTime.Parse("26 08 2022 18:00:00"),
                 Type = koncert,
-                ImagePath = "/Images/1.jpg"
+                ImagePath = "/Images/01.png",
+                Price = 249
             };
 
             var arcticMonkeys = new Event
@@ -71,16 +71,19 @@ namespace TicketsShop.Models
                 Name = "Alibi Od Zaraz",
                 CityId = warszawa.Id,
                 CreationTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
-                Description = "None",
-                EventTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
-                Type = koncert,
-                ImagePath = "/Images/1.jpg"
+                Description = "Przyłapany na zdradzie mąż potrafi wznieść się na wyżyny kreatywności! Sztuka Jeana Poireta “Alibi od zaraz” jest tego niezbitym dowodem. Michel, zaprasza do domu piękną młodą dziewczynę.Gdy powoli wciela w życie swoje marzenia o romansie, w drzwiach staje jego żona Sophie!Zdezorientowany i przerażony wizją rozwodu z dobrze sytuowaną małżonką, potrzebuje porządnego alibi. Na poczekaniu wymyśla niedorzeczną historię i wciąga niedoszłą kochankę do wspólnego odgrywania absolutnie nieprzemyślanej farsy…Jakie alibi będzie najbardziej wiarygodne? W co uwierzy żona? Scenariusza komicznej intrygi nie zna nikt! Nawet sam autor!",
+                EventTime = DateTime.Parse("TimeSpan.FromDays(2)"),
+                Type = teatr,
+                ImagePath = "/Images/02.png",
+                Price = 142,
+                PlaceName = "Teatr Capitol"
             };
+
 
             var kota = new Event
             {
                 Name = "Kiedy Kota Niema",
-                CityId = katowice.Id,
+                CityId = poznan.Id,
                 CreationTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
                 Description = "None",
                 EventTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
@@ -91,7 +94,7 @@ namespace TicketsShop.Models
             var psa = new Event
             {
                 Name = "Kiedy Psa Niema",
-                CityId = katowice.Id,
+                CityId = poznan.Id,
                 CreationTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
                 Description = "None",
                 EventTime = DateTime.Now.Add(TimeSpan.FromDays(2)),
